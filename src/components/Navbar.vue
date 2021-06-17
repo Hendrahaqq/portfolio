60acfbe4667884c99089bfa52320a96bf9b556a3
Muhammad Hendra Nailul Haqq <hendrahaqq@gmail.com>
2021-06-17 15:10:27 +0700

    add changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,23 +2,23 @@
  <div class="font-sans bg-gray-900 text-white fixed w-full">
   <nav class="flex items-center justify-between flex-wrap p-4">
     <a href="#hero" class="flex items-center flex-no-shrink text-white mr-6" v-smooth-scroll>
-      <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-      <span class="font-semibold text-xl tracking-tight">HENDRA</span>
+      <svg class="fill-current bg-green-500 p-1 rounded-full h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+      <span class="font-semibold text-xl text-green-500 tracking-tight">HENDRA</span>
     </a>
     <div class="block sm:hidden">
       <button @click="toggle" class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
     </div>
-    <div :class="open ? 'inline-block': 'hidden'" class="w-full text-right flex-grow sm:flex sm:items-center sm:w-auto">
+    <div :class="open ? 'inline-block': 'hidden'" class="w-full text-right flex-grow sm:flex sm:items-center sm:w-auto ">
       <div class="text-sm sm:flex-grow">
-        <a href="#about" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-teal-lighter hover:text-white mx-5" v-smooth-scroll>
+        <a href="#about" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-green-500 hover:text-white mx-5" v-smooth-scroll>
           About
         </a>
-        <a href="#skill" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-teal-lighter hover:text-white mx-5" v-smooth-scroll>
+        <a href="#skill" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-green-500 hover:text-white mx-5" v-smooth-scroll>
           Skill
         </a>
-        <a href="#work" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-teal-lighter hover:text-white mx-5" v-smooth-scroll>
+        <a href="#work" class="no-underline block mt-4 sm:inline-block sm:mt-0 text-green-500 hover:text-white mx-5" v-smooth-scroll>
           Work
         </a>
       </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,73 +1,87 @@
 <template>
- <div id="hero"  class="hero-header pt-24 min-h-screen flex justify-items-center place-items-center place-content-center">
-      <div class="div">
+    <div id="hero"
+        class="hero-header pt-24 min-h-screen flex justify-items-center place-items-center place-content-center">
+        <div class="div">
 
-        <div class="text-center p-5 bg-gray-900 bg-opacity-80 rounded-md">
-           <img src="/src/assets/rai.png" class="rai-img mx-auto" alt="">
-            <p class="font-bold mt-10 text-xl 2xl:text-3xl text-gray-800 bg-white p-2 rounded-md">Muhammad Hendra Nailul Haqq</p>
-            <p class="text-lg 2xl:text-2xl mt-1  font-source">Front End Developer</p>
-            <div class="grid grid-cols-3 mt-10 w-3/4 mx-auto">
-            <a href="" class="text-center icon-hero">
-                <fa :icon="['fab', 'gitlab']" class="text-4xl " />
-                <p>Git</p>
-            </a>
-              <a href="" class="text-center icon-hero ">
-                
-                 <fa icon="envelope-open-text" class="text-4xl" />
-                 <p>Email</p>
-                                
-              </a>
-             <a href="" class="text-center icon-hero">
-                 <fa :icon="['fab','linkedin-in']" class="text-4xl " />
-                 <p>LinkedIn</p>
-             </a>
-             
-             
+            <div class="text-center p-5 bg-gray-900 bg-opacity-80 rounded-md">
+                <img src="/src/assets/rai.png" class="rai-img mx-auto" alt="">
+                <p class="font-bold mt-10 text-xl 2xl:text-3xl text-gray-800 bg-green-500 p-2 rounded-md">Muhammad Hendra
+                    Nailul Haqq</p>
+                <p class="text-lg 2xl:text-2xl mt-1 text  font-source">Front End Developer</p>
+                <div class="grid grid-cols-3 mt-10 w-3/4 mx-auto">
+                    <a href="" class="text-center icon-hero">
+                        <fa :icon="['fab', 'gitlab']" class="text-4xl " />
+                        <p>Git</p>
+                    </a>
+                    <a href="" class="text-center icon-hero ">
+
+                        <fa icon="envelope-open-text" class="text-4xl" />
+                        <p>Email</p>
+
+                    </a>
+                    <a href="" class="text-center icon-hero">
+                        <fa :icon="['fab','linkedin-in']" class="text-4xl " />
+                        <p>LinkedIn</p>
+                    </a>
+
+
+                </div>
             </div>
-      </div>
-      <div class="text-center mt-10">
-          <!-- <p class="text-gray-900 text-xl font-bold mb-0">Know me more</p> -->
-          <a href="#skill">
-              <div class="arrow bounce"></div>
-          </a>
-          
-      </div>
-    
+            <div class="text-center mt-10">
+                <!-- <p class="text-gray-900 text-xl font-bold mb-0">Know me more</p> -->
+                <a href="#about" v-smooth-scroll>
+                    <div class="arrow bounce"></div>
+                </a>
 
-      </div>
-      
-       
+            </div>
+
+
+        </div>
+
+
     </div>
-<div class="bg-gray-800 min-h-screen w-full p-10 ">
+    <div id="about" class="bg-gray-800 min-h-screen w-full p-10 ">
 
-   
 
-    <div id="about" class="grid grid-cols-2 bg-gray-700 shadow-2xl p-10 pt-14 md:w-3/4 mx-auto mt-5 min-h-screen">
-        <div class="p-5">
-          <p class="font-bold text-white ">About</p>
+
+        <div  class=" bg-gray-700 shadow-2xl p-10 md:w-3/4 mx-auto mt-5 min-h-screen">
+            <div class="border-4 border-white relative">
+                <div class="bg-gray-700 flex items-center absolute -top-7 left-2 p-2">
+                    <fa icon="info-circle" class="text-4xl mr-2" />
+                    <span class="font-bold  text-green-500 "> About Me</span>
+                </div>
+
+                <div class="grid grid-cols-2">
+                    <div class="p-5">
+
+
+                    </div>
+                    <div class=" p-5">
+
+                    </div>
+                </div>
+
+            </div>
+
         </div>
-        <div class=" p-5">
 
+        <div id="skill" class="grid grid-cols-2 bg-gray-900 shadow-2xl p-10 pt-14 md:w-3/4 mx-auto mt-5 min-h-screen">
+            <div class="p-5">
+                <p class="font-bold text-white ">My Work</p>
+            </div>
+            <div class=" p-5">
+
+            </div>
         </div>
+
     </div>
 
-    <div id="skill" class="grid grid-cols-2 bg-gray-900 shadow-2xl p-10 pt-14 md:w-3/4 mx-auto mt-5 min-h-screen">
-        <div class="p-5">
-          <p class="font-bold text-white ">My Work</p>
-        </div>
-        <div class=" p-5">
-
-        </div>
-    </div>
-   
-</div>
- 
 </template>
 
 <script>
-export default {
+    export default {
 
-}
+    }
 </script>
 
 <style>
