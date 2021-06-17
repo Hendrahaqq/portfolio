3c36642683e68b4903d4e7ee7cf2b1df478b222d
Muhammad Hendra Nailul Haqq <hendrahaqq@gmail.com>
2021-06-17 21:29:05 +0700

    add changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,9 @@
 <template>
- <div class="font-sans bg-gray-900 text-white fixed w-full">
+ <div class="font-sans bg-gray-900 text-white fixed w-full z-50">
   <nav class="flex items-center justify-between flex-wrap p-4">
     <a href="#hero" class="flex items-center flex-no-shrink text-white mr-6" v-smooth-scroll>
-      <svg class="fill-current bg-green-500 p-1 rounded-full h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+      <img src="/src/assets/hh.png" width="40" height="40" class="mr-2" alt="">
+      <!-- <svg class="fill-current bg-green-500 p-1 rounded-full h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> -->
       <span class="font-semibold text-xl  text-green-500 tracking-tight">HENDRA</span>
     </a>
     <div class="block sm:hidden">
